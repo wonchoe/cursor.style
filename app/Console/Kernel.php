351e5f97454d593b2b234e8d7c990d11005be13a
Oleksii <oleksisem@gmail.com>
2024-09-26 11:29:52 -0400

    sss

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel {
     protected function schedule(Schedule $schedule) {
 //        $schedule->command('quote:daily')->daily();
 //        $schedule->command('randomTop')->daily();
-        $schedule->command('getExtensionData')->everySixHours($minutes = 0);
+        $schedule->command('getExtensionData')->hourly();
         $schedule->command('yandex:report')->everyFiveMinutes();
     }
 
