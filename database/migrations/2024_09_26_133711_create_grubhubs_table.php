984c9e08fbd819bfd4bbc2986959219c05e455ef
Oleksii <oleksisem@gmail.com>
2024-09-26 14:18:10 -0400

    gh

--- database/migrations/2024_09_26_133711_create_grubhubs_table.php
+++ database/migrations/2024_09_26_133711_create_grubhubs_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id(); 
             $table->string('at');
             $table->string('rt'); 
-            $table->date('updated'); 
+            $table->datetime('updated'); 
         });
     }
 
