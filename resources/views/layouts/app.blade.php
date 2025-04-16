626f43977fd3ee8803c6e40d6dae7d966611d0c4
Oleksii <oleksisem@gmail.com>
2025-04-16 13:20:35 -0400

    google analytic added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    @yield('page_meta')    
+    @yield('page_meta')
     <link rel="alternate" hreflang="x-default" href="https://cursor.style/{{ltrim(Request::path(), '/')}}" />
     <link rel="alternate" hreflang="es" href="https://es.cursor.style/{{ltrim(Request::path(), '/')}}" />
     <link rel="alternate" hreflang="ru" href="https://ru.cursor.style/{{ltrim(Request::path(), '/')}}" />
@@ -22,46 +22,61 @@
     <link rel="stylesheet" href="{{ secure_asset('css/mycollection.css') }}" />
     <link rel="stylesheet" href="{{ secure_asset('css/main_updated.css') }}" />
     <link rel="stylesheet" href="{{ secure_asset('/css/chat.css')}}" />
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Material+Icons|Material+Icons+Round" />    
-    <link href="https://fonts.googleapis.com/css2?family=Shantell+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Noto+Color+Emoji&family=Shantell+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Material+Icons|Material+Icons+Round" />
+    <link href="https://fonts.googleapis.com/css2?family=Shantell+Sans:ital,wght@0,300..800;1,300..800&display=swap"
+        rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Noto+Color+Emoji&family=Shantell+Sans:ital,wght@0,300..800;1,300..800&display=swap"
+        rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap" rel="stylesheet">
-    
+
     <link rel="canonical" href="https://{{ Request::host() }}{{ Request::getRequestUri() }}" />
 
     @yield('lib_top')
-    <script async src="{{ secure_asset('/js/lang.v1.js')}}"></script>    
+    <script async src="{{ secure_asset('/js/lang.v1.js')}}"></script>
     <script src="{{ secure_asset('/js/init.js')}}"></script>
+
+
+    <!-- Google Analytics tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-Z6S2NMJGYR"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag() { dataLayer.push(arguments); }
+        gtag('js', new Date());
+
+        gtag('config', 'G-Z6S2NMJGYR');
+    </script>
+
+
+
+    @if (request()->is('success') || request()->is('feedback'))
+        <!-- Yandex.Metrika counter -->
+        <script type="text/javascript">
+            (function (m, e, t, r, i, k, a) {
+                m[i] = m[i] || function () { (m[i].a = m[i].a || []).push(arguments) };
+                m[i].l = 1 * new Date();
+                for (var j = 0; j < document.scripts.length; j++) { if (document.scripts[j].src === r) { return; } }
+                k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)
+            })
+                (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+            ym(91861405, "init", {
+                clickmap: true,
+                trackLinks: true,
+                accurateTrackBounce: true
+            });
+        </script>
+        <noscript>
+            <div><img src="https://mc.yandex.ru/watch/91861405" style="position:absolute; left:-9999px;" alt="" /></div>
+        </noscript>
+        <!-- /Yandex.Metrika counter -->
+    @endif
+
+    
 
 </head>
 
 <body>
-
-
-
-@if (request()->is('success') || request()->is('feedback'))
-    <!-- Yandex.Metrika counter -->
-    <script type="text/javascript">
-       (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-       m[i].l=1*new Date();
-       for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-       k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-       (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-
-       ym(91861405, "init", {
-            clickmap:true,
-            trackLinks:true,
-            accurateTrackBounce:true
-       });
-    </script>
-    <noscript>
-        <div><img src="https://mc.yandex.ru/watch/91861405" style="position:absolute; left:-9999px;" alt="" /></div>
-    </noscript>
-    <!-- /Yandex.Metrika counter -->
-@endif
-
-
-
 
     <div id="ovelay" class="overlay" style="display:none"></div>
 
@@ -138,7 +153,8 @@
         <div class="footer">
             <div class="container">
                 <div class="copyright">
-                    © 2019–{{ now()->year }} 🖱️ Cursor.Style — by <a href="https://www.tiktok.com/@wonchoe" target="_blank">Oleksii Semeniuk</a>
+                    © 2019–{{ now()->year }} 🖱️ Cursor.Style — by <a href="https://www.tiktok.com/@wonchoe"
+                        target="_blank">Oleksii Semeniuk</a>
                 </div>
                 <div class="footer__links">
                     <div><a class="downlink" href="/terms">@lang('messages.footer_term_of_use')</a></div>
