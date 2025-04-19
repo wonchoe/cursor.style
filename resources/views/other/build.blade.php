07e5c15238eb28c7bbd10880c5620e5f3feb25d9
Oleksii <oleksisem@gmail.com>
2025-04-18 23:28:35 -0400

    added ::has css style to hide effect tab if the effects off

--- resources/views/other/build.blade.php
+++ resources/views/other/build.blade.php
@@ -1,7 +1,7 @@
 @php
     if (!function_exists('build_version')) {
         function build_version() {
-            return '?v=7'; // ← тут змінюй версію вручну або прочитай з файлу
+            return '?v=8'; // ← тут змінюй версію вручну або прочитай з файлу
         }
     }
 @endphp
