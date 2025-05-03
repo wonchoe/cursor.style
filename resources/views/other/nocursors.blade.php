dc57f5b760e03f82455547ec578b0fa288740faf
Oleksii <oleksisem@gmail.com>
2025-05-03 17:52:47 -0400

    t

--- resources/views/other/nocursors.blade.php
+++ resources/views/other/nocursors.blade.php
@@ -3,16 +3,27 @@
   type="module"
 ></script>
 
-<div class="main__item"><div class="install-panel__title">More cursors every day! Stay with us!</div>
-<div class="main__item-img">
-<dotlottie-player
-  src="https://lottie.host/34444af9-139e-49fb-a950-f6db8e27c3a9/7ObBdzCmRI.lottie"
-  background="transparent"
-  speed="1"
-  style="width: 300px; height: 300px"
-  loop
-  autoplay
-></dotlottie-player>    
-
+<div class="">
+    <div class="install-panel__title">More cursors every day! Stay with us!</div>
+<div class="under-const" style="background: white;
+    border-radius: 12px;
+    padding: 16px;
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.05);
+    transition: transform 0.2s;
+    display: flex
+;
+    align-items: center;
+    align-content: center;
+    justify-content: center;
+    max-height: 200px;
+    flex-direction: column;">
+    <dotlottie-player
+    src="https://lottie.host/34444af9-139e-49fb-a950-f6db8e27c3a9/7ObBdzCmRI.lottie"
+    background="transparent"
+    speed="1"
+    style="width: 300px; height: 300px"
+    loop
+    autoplay
+    ></dotlottie-player>    
 </div>
 </div>
