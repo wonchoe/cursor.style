d5dfad7f9f103f4ec7309f1a913e3a40363d0ac5
Oleksii <oleksisem@gmail.com>
2025-05-03 17:50:58 -0400

    t

--- resources/views/other/nocursors.blade.php
+++ resources/views/other/nocursors.blade.php
@@ -1,3 +1,18 @@
+<script
+  src="https://unpkg.com/@dotlottie/player-component@2.7.12/dist/dotlottie-player.mjs"
+  type="module"
+></script>
+
 <div class="main__item"><div class="install-panel__title">More cursors every day! Stay with us!</div>
-<div class="main__item-img"><img src="/images/logo_300-300.png"></div>
+<div class="main__item-img">
+<dotlottie-player
+  src="https://lottie.host/34444af9-139e-49fb-a950-f6db8e27c3a9/7ObBdzCmRI.lottie"
+  background="transparent"
+  speed="1"
+  style="width: 300px; height: 300px"
+  loop
+  autoplay
+></dotlottie-player>    
+
+</div>
 </div>
