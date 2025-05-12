1d23a92049181e4d4f975944c30000d2e96efa64
Oleksii <oleksisem@gmail.com>
2025-05-12 10:11:30 -0400

    w

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -14,6 +14,7 @@ class VerifyCsrfToken extends Middleware
     protected $except = [
         '/contact',
         '/feedback',
-        '/update/set'
+        '/update/set',
+        '/search'
     ];
 }
