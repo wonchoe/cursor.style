78a1aedb4916c03d05a3fdd1c16baaeba66ba5fb
Oleksii <oleksisem@gmail.com>
2025-05-14 00:53:01 -0400

    e

--- resources/views/allcat.blade.php
+++ resources/views/allcat.blade.php
@@ -60,7 +60,7 @@
                            {{ $item->currentTranslation->name ?? $item->base_name_en }}
                         </h2>
                     <img class="main__cat-img"
-                        src="collection/{{ $item->id }}-{{ $item->alt_name }}"
+                        src="collection/{{ $item->id }}-{{ $item->alt_name }}.png"
                         alt="{{ $item->currentTranslation->name ?? $item->base_name_en }}"
                         title="{{ $item->currentTranslation->short_desc ?? $item->short_descr }}">
                     </a>
