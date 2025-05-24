2c9461479c783392c33c178a8f76e10ae164348c
oleksii <oleksisem@gmail.com>
2025-05-24 19:21:10 -0400

    access to public

--- resources/views/partials/review.blade.php
+++ resources/views/partials/review.blade.php
@@ -1,15 +1,14 @@
 <script
-  src="{{ asset_ver('/js/dotlottie-player.mjs') }}"
+  src="https://unpkg.com/@dotlottie/player-component@2.7.12/dist/dotlottie-player.mjs"
   type="module"
 ></script>
-
 
 <a href="https://chromewebstore.google.com/detail/cursor-style-–-custom-mou/bmjmipppabdlpjccanalncobmbacckjn/reviews?hl=en&authuser=2" target="_blank">
 <div class="review-container">
     <div class="review-text">@lang('messages.leave_review')</div>
     <div>
         <dotlottie-player
-        src="{{ asset_ver('/images/reward.lottie') }}"
+        src="/images/reward.lottie"
         background="transparent"
         speed="1"
         style="width: 200px; height: 200px"
