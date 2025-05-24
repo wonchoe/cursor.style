f0b1568f5f5aab0a00a4e099431975ba64462e9c
oleksii <oleksisem@gmail.com>
2025-05-24 17:43:39 -0400

    access to public

--- public/js/init.js
+++ public/js/init.js
@@ -311,7 +311,7 @@ function setupMiliSearch() {
       html += `
         <div class="category-block" style="margin-bottom:16px;">
           <div class="category-header" onclick="window.location='/collections/${group.alt}'" style="display:flex;align-items:center;margin-bottom:5px;">
-            ${group.img ? `<img src="${window.assetCdnBase}${group.img}" height="32">` : ''}
+            ${group.img ? `<img src="${window.assetCdnBase}/${group.img}" height="32">` : ''}
             <strong>${group.name}</strong>
           </div>
           <ul style="list-style:none;padding-left:10px;margin:0;">
