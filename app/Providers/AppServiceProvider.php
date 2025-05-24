f2264c654a5642371db55884430d9ec80973a7ac
oleksii <oleksisem@gmail.com>
2025-05-24 18:59:32 -0400

    access to public

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Pagination\Paginator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,21 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        date_default_timezone_set("America/New_York");
+        Paginator::defaultView('vendor/pagination/bootstrap-4');
+        URL::forceScheme('https');        
+
+        $host = request()->getHost(); // наприклад, uk.cursor.style
+        $parts = explode('.', $host);
+        $potentialLang = $parts[0];
+
+        $availableLocales = [
+            'en', 'am', 'ar', 'bg', 'bn', 'ca', 'cs', 'da', 'de', 'el', 'es', 'et', 'fa', 'fi', 'fil', 'fr', 'gu', 'he',
+            'hi', 'hr', 'hu', 'id', 'it', 'ja', 'kn', 'ko', 'lt', 'lv', 'ml', 'mr', 'ms', 'nl', 'no', 'pl', 'pt', 'ro', 'ru',
+            'sk', 'sl', 'sr', 'sv', 'sw', 'ta', 'te', 'th', 'tr', 'uk', 'vi', 'zh'
+        ];        
+        if (in_array($potentialLang, $availableLocales)) {
+            app()->setLocale($potentialLang);
+        }        
     }
 }
