0e03c88f5683d181cb869b4f36e818203d17f489
oleksii <oleksisem@gmail.com>
2025-05-24 19:57:29 -0400

    access to public

--- resources/views/partials/cursor-list.blade.php
+++ resources/views/partials/cursor-list.blade.php
@@ -16,8 +16,8 @@
                 <p>{{ $translation }}</p>
             </div>
             <div class="main__item-img cs_pointer" data-cur-id="{{ $cursor->id }}" cursorshover="true">
-                <img loading="lazy" class="cursorimg" style="cursor: url({{ asset_cdn($cursor->c_file) }}) 0 0, auto !important;" src="{{ asset_cdn($cursor->c_file) }}">
-                <img loading="lazy" class="cursorimg" style="cursor: url({{ asset_cdn($cursor->p_file) }}) 0 0, auto !important;" src="{{ asset_cdn($cursor->p_file) }}">
+                <img alt="{{ $translation }}" title="{{ $translation }}" loading="lazy" class="cursorimg" style="cursor: url({{ asset_cdn($cursor->c_file) }}) 0 0, auto !important;" src="{{ asset_cdn($cursor->c_file) }}">
+                <img alt="{{ $translation }}" title="{{ $translation }}" loading="lazy" class="cursorimg" style="cursor: url({{ asset_cdn($cursor->p_file) }}) 0 0, auto !important;" src="{{ asset_cdn($cursor->p_file) }}">
             </div>
 
             <span class="downloads-badge">
