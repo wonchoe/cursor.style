3326c07e61d5c8952927d0971d37d07521a4326c
oleksii <oleksisem@gmail.com>
2025-05-28 15:09:52 -0400

    schedule

--- routes/console.php
+++ routes/console.php
@@ -8,21 +8,21 @@ use Illuminate\Support\Facades\Schedule;
 
 Schedule::command('adsense:fetch')->everyFiveMinutes();
 Schedule::command('custom:ChromStats')->hourly();
-Schedule::command('custom:GetGoogleAnalyticsData')->everyMinute();
+Schedule::command('custom:GetGoogleAnalyticsData')->everyFiveMinutes();
 
 // Create translation
-Schedule::command('custom:translate-collections')->everyFiveMinutes();
-Schedule::command('custom:TranslateCursor')->everyFiveMinutes();
+Schedule::command('custom:translate-collections')->everyThirtyMinutes();
+Schedule::command('custom:TranslateCursor')->everyThirtyMinutes();
 
 // Create tags for SEO
-Schedule::command('custom:tagsCreate')->everyFiveMinutes();
+Schedule::command('custom:tagsCreate')->everyThirtyMinutes();
 
 // Add to search
-Schedule::command('custom:meilisearchAddCursors')->everyFiveMinutes();
+Schedule::command('custom:meilisearchAddCursors')->everyThirtyMinutes();
 
-Schedule::command('custom:GetCursorClickStats')->everyFiveMinutes();
+Schedule::command('custom:GetCursorClickStats')->everyThirtyMinutes();
 
 // SEO chatGPT
-Schedule::command('custom:seoSiscovery')->everyFiveMinutes();
-Schedule::command('custom:seoBatchPrepareSend')->everyFiveMinutes();
-Schedule::command('custom:seoBatchFetchSave')->everyFiveMinutes();
+Schedule::command('custom:seoSiscovery')->everyThirtyMinutes();
+Schedule::command('custom:seoBatchPrepareSend')->everyThirtyMinutes();
+Schedule::command('custom:seoBatchFetchSave')->everyThirtyMinutes();
