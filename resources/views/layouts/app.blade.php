cbe64087e229688e8dad2ae929d56869277923ab
oleksii <oleksisem@gmail.com>
2025-06-08 21:18:23 -0400

    rights

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,8 +18,8 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 
 
-    <link rel="stylesheet" href="/css/styles.css?v=37" media="print" onload="this.media='all'">
-    <link rel="stylesheet" href="/css/critical.css?v=37">
+    <link rel="stylesheet" href="{{ asset_ver('/css/styles.css') }}" media="print" onload="this.media='all'">
+    <link rel="stylesheet" href="{{ asset_ver('/css/critical.css') }}">
     <link rel="stylesheet" href="{{ asset_ver('css/chat.css') }}" media="print" onload="this.media='all'" />
     <link href="https://fonts.googleapis.com/css2?family=Shantell+Sans&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap" rel="stylesheet">
