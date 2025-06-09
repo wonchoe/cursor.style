f921739b15968ed97b1b97a3433544ce14bb02be
oleksii <oleksisem@gmail.com>
2025-06-08 21:23:14 -0400

    first cursors not lazy

--- resources/views/partials/cursor-list.blade.php
+++ resources/views/partials/cursor-list.blade.php
@@ -11,17 +11,23 @@
             $slug = Str::slug($translation) ?: Str::slug($cursor->name_en);
         @endphp
 
-        <div class="main__item" data-container-id="{{ $cursor->id }}" onclick="handleItemClick(event, '{{ $cursor->details_url }}')">
+        <div class="main__item" data-container-id="{{ $cursor->id }}"
+            onclick="handleItemClick(event, '{{ $cursor->details_url }}')">
             <div class="div_ar_p">
                 <p>{{ $translation }}</p>
             </div>
             <div class="main__item-img cs_pointer" data-cur-id="{{ $cursor->id }}" cursorshover="true">
-                <img alt="{{ $translation }}" title="{{ $translation }}" loading="lazy" class="cursorimg" style="cursor: url({{ asset_cdn($cursor->c_file) }}) 0 0, auto !important;" src="{{ asset_cdn($cursor->c_file) }}">
-                <img alt="{{ $translation }}" title="{{ $translation }}" loading="lazy" class="cursorimg" style="cursor: url({{ asset_cdn($cursor->p_file) }}) 0 0, auto !important;" src="{{ asset_cdn($cursor->p_file) }}">
+                <img alt="{{ $translation }}" title="{{ $translation }}" loading="{{ $key < 3 ? 'eager' : 'lazy' }}"
+                    class="cursorimg" style="cursor: url({{ asset_cdn($cursor->c_file) }}) 0 0, auto !important;"
+                    src="{{ asset_cdn($cursor->c_file) }}">
+                <img alt="{{ $translation }}" title="{{ $translation }}" loading="{{ $key < 3 ? 'eager' : 'lazy' }}"
+                    class="cursorimg" style="cursor: url({{ asset_cdn($cursor->p_file) }}) 0 0, auto !important;"
+                    src="{{ asset_cdn($cursor->p_file) }}">
             </div>
 
             <span class="downloads-badge">
-                <img src="{{ asset_cdn('images/icons/download.png') }}" alt="Downloads" title="Downloads" class="download-count-img">
+                <img src="{{ asset_cdn('images/icons/download.png') }}" alt="Downloads" title="Downloads"
+                    class="download-count-img">
                 {{ number_format($cursor->totalClick + $cursor->todayClick) }}
             </span>
 
