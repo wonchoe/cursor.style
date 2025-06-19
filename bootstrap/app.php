7586f1582b75c71fa64745c2d4ce1048c8d8b582
Oleksii Semeniuk <oleksisem@gmail.com>
2025-06-19 18:10:59 +0000

    slash fixed

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->validateCsrfTokens(except: [
             '/search',
-            'chatlogs'
+            '/chatlogs'
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
